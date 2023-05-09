8155693a5dee3d13204ffbf7aba5a9e2522ebe36
Anooj Singh <anujsingh49244@gmail.com>
2023-05-09 17:26:34 +0530

    Add animation

--- src/components/resources/index.tsx
+++ src/components/resources/index.tsx
@@ -1,14 +1,84 @@
+import { useLayoutEffect } from "react";
 import { layout } from "../../styles";
 import { RESOURCE_LIST } from "./constants";
+import { gsap } from "gsap";
 
-const Resources: React.FC<{ mainRef: React.MutableRefObject<null> }> = () => {
+const Resources: React.FC<{ mainRef: React.MutableRefObject<null> }> = (
+  props
+) => {
+  useLayoutEffect(() => {
+    const ctx = gsap.context(() => {
+      gsap
+        .timeline({
+          scrollTrigger: {
+            trigger: "#section_resource",
+            start: "top top",
+            end: "+=3500",
+            scrub: 1,
+            pin: true,
+          },
+        })
+        .from("#section_resource .div1 h1", {
+          y: window.innerWidth * 1,
+          opacity: 0,
+          scale: 0.5,
+        })
+        .from("#section_resource .div1 p", {
+          y: window.innerWidth * 1,
+          opacity: 0,
+          scale: 0.5,
+        })
+        .from("#section_resource .div2 .label1", {
+          x: window.innerWidth * -1,
+          opacity: 0,
+          scale: 0.1,
+        })
+        .from("#section_resource .div2 .img1", {
+          y: window.innerWidth * 1,
+          opacity: 0,
+          scale: 0.5,
+        })
+        .from("#section_resource .div2 .label2", {
+          x: window.innerWidth * -1,
+          opacity: 0,
+          scale: 0.1,
+        })
+        .from("#section_resource .div2 .img2", {
+          y: window.innerWidth * 1,
+          opacity: 0,
+          scale: 0.5,
+        })
+        .from("#section_resource .div2 .label3", {
+          x: window.innerWidth * -1,
+          opacity: 0,
+          scale: 0.1,
+        })
+        .from("#section_resource .div2 .img3", {
+          y: window.innerWidth * 1,
+          opacity: 0,
+          scale: 0.5,
+        })
+        .from("#section_resource .div2 .label4", {
+          x: window.innerWidth * -1,
+          opacity: 0,
+          scale: 0.1,
+        })
+        .from("#section_resource .div2 .img4", {
+          y: window.innerWidth * 1,
+          opacity: 0,
+          scale: 0.5,
+        });
+    }, props.mainRef); // <- Scope!
+    return () => ctx.revert(); // <- Cleanup!
+  }, []);
+
   return (
     <section
       id="section_resource"
-      className={`${layout.flex.directionCol.itemsCenter} gap-14 bg-blue-100 py-20`}
+      className={`${layout.flex.directionCol.itemsCenter} gap-20 bg-blue-100 py-20 overflow-hidden`}
     >
       <div
-        className={`${layout.flex.directionCol.itemsJustifyCenter} w-full gap-8 h-1/4`}
+        className={`div1 ${layout.flex.directionCol.itemsJustifyCenter} w-full gap-8 h-1/4`}
       >
         <h1 className="text-6xl font-semibold">Resources for Builders</h1>
         <p className="text-2xl w-1/2 px-5 text-center">
@@ -18,13 +88,15 @@ const Resources: React.FC<{ mainRef: React.MutableRefObject<null> }> = () => {
         </p>
       </div>
       <div
-        className={`${layout.flex.directionRow.itemCenterSpaceBetween} w-[85%] h-3/4 `}
+        className={`div2 ${layout.flex.directionRow.itemCenterSpaceBetween} w-[85%] h-3/4 `}
       >
         <div className="w-1/2">
           <ul>
-            {RESOURCE_LIST.map((eachResource) => (
+            {RESOURCE_LIST.map((eachResource, index) => (
               <li
-                className={`${layout.flex.directionRow.itemsCenterJustifyStart} gap-5 border-b border-b-blue-500 py-7 w-max`}
+                className={`label${index + 1} ${
+                  layout.flex.directionRow.itemsCenterJustifyStart
+                } gap-5 border-b border-b-blue-500 py-7 w-max`}
               >
                 <span className="text-5xl">{eachResource.preIcon}</span>
                 <h1 className="text-4xl font-semibold">{eachResource.label}</h1>
@@ -35,8 +107,8 @@ const Resources: React.FC<{ mainRef: React.MutableRefObject<null> }> = () => {
         <div
           className={`${layout.flex.directionRow.itemJustifyCenter} content-around w-1/2 h-full flex-wrap gap-5`}
         >
-          {RESOURCE_LIST.map((eachResource) => (
-            <div className={`${eachResource.className}`}>
+          {RESOURCE_LIST.map((eachResource, index) => (
+            <div className={`img${index + 1} ${eachResource.className}`}>
               <div
                 className={`${layout.flex.directionRow.itemJustifyCenter} w-full h-full rounded-xl border-2 border-blue-500 overflow-hidden p-10`}
               >
